d4108d0c95617ed4818c349f2fd0a32fdd15d870
Joep Stender <joep@stender.nl>
2016-07-31 14:53:42 +0200

    Use older dependencies

--- mix.exs
+++ mix.exs
@@ -4,13 +4,13 @@ defmodule Peepchat.Mixfile do
   def project do
     [app: :peepchat,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     aliases: aliases(),
-     deps: deps()]
+     aliases: aliases,
+     deps: deps]
   end
 
   # Configuration for the OTP application.
@@ -18,8 +18,8 @@ defmodule Peepchat.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Peepchat, []},
-     applications: [:phoenix, :phoenix_pubsub, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+     applications: [:phoenix, :cowboy, :logger, :gettext,
+                    :phoenix_ecto, :postgrex, :comeonin]]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,16 +30,18 @@ defmodule Peepchat.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
+    [{:phoenix, "~> 1.1.2"},
+     {:phoenix_ecto, "~> 2.0"},
      {:postgrex, ">= 0.0.0"},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:cors_plug, "~> 1.1"}]
+     {:gettext, "~> 0.9"},
+     {:cors_plug, "~> 1.1"},
+     {:ja_serializer, "~> 0.8.1"},
+     {:comeonin, "~> 2.4"},
+     {:guardian, "~> 0.10.0"},
+     {:cowboy, "~> 1.0"}]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
+  # Aliases are shortcut or tasks specific to the current project.
   # For example, to create, migrate and run the seeds file at once:
   #
   #     $ mix ecto.setup
@@ -47,7 +49,6 @@ defmodule Peepchat.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-     "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     "ecto.reset": ["ecto.drop", "ecto.setup"]]
   end
 end
